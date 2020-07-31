c3f18929062556cb1de09751d6bc2de53cab21af
SeoHyeonMyeong <gusaud2199@naver.com>
2020-07-31 16:30:13 +0900

    강화 액티비티에 RecyclerView 적용, 강화 RecyclerView 전용 adapter 생성, adapter에 들어갈 Reinforce_view 생성, gradle에 RecylcerView 라이브러리 추가

---
+++ app/src/main/java/com/test/rainbowDefense/adapter/ReinforceAdapter.kt
@@ -0,0 +1,44 @@
+package com.test.rainbowDefense.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.view.marginBottom
+import androidx.core.view.marginStart
+import androidx.recyclerview.widget.RecyclerView
+import com.test.rainbowDefense.R
+
+class ReinforceAdapter(private val myDataset: Array<String>) :
+    RecyclerView.Adapter<ReinforceAdapter.MyViewHolder>() {
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder.
+    // Each data item is just a string in this case that is shown in a TextView.
+    class MyViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
+
+    // Create new views (invoked by the layout manager)
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ReinforceAdapter.MyViewHolder {
+        // create a new view
+        val textView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.reinforce_view, parent, false) as TextView
+        // set the view's size, margins, paddings and layout parameters
+        textView.setTextSize(10f)
+        TODO()
+        return MyViewHolder(textView)
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        holder.textView.text = myDataset[position]
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    override fun getItemCount() = myDataset.size
+}
+
--- app/src/main/java/com/test/rainbowDefense/reinforce.kt
+++ app/src/main/java/com/test/rainbowDefense/reinforce.kt
@@ -4,9 +4,17 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.test.rainbowDefense.adapter.ReinforceAdapter
 import kotlinx.android.synthetic.main.activity_reinforce.*
 
 class reinforce : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewManager: RecyclerView.LayoutManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reinforce)
@@ -20,8 +28,26 @@ class reinforce : AppCompatActivity() {
 
         val intent = Intent(this, loby::class.java)
 
+
         btn_reinforce_back.setOnClickListener{view ->
             startActivity(intent)
+        }
+
+        val myDataset = arrayOf("현명","문성","진우","진수","태웅","소연")
+        viewManager = LinearLayoutManager(this)
+        viewAdapter = ReinforceAdapter(myDataset)
+
+        recyclerView = findViewById<RecyclerView>(R.id.recycler_view).apply {
+            // use this setting to improve performance if you know that changes
+            // in content do not change the layout size of the RecyclerView
+            setHasFixedSize(true)
+
+            // use a linear layout manager
+            layoutManager = viewManager
+
+            // specify an viewAdapter (see also next example)
+            adapter = viewAdapter
+
         }
     }
 }
