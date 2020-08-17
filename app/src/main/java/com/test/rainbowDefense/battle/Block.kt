883be4d40ddaecebafbbad4a36bc6ea44b111a5f
SeoHyeonMyeong <gusaud2199@naver.com>
2020-08-17 22:23:55 +0900

    Block 클래스 추가
    유닛블록, 건물블록, 스킬블록의 부모클래스가 될 Block 클래스 추가,
    Battle 액티비티 내부에서 하단에 자리할 BlockMenu 클래스 추가,
    모든 Block 인스턴스는 BlockMenu 내부에서만 생성되도록 변경

---
+++ app/src/main/java/com/test/rainbowDefense/battle/Block.kt
@@ -0,0 +1,40 @@
+package com.test.rainbowDefense.battle
+
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
+
+class Block(
+    x: Int,
+    y: Int,
+    width: Int,
+    height: Int,
+    drawable: Drawable,
+    clickDrawable : Drawable
+)
+    : Shape(x,y,width,height,drawable){
+
+    var isClickable = true
+    var isClick = false
+    private val clickBitmapDrawable : BitmapDrawable = clickDrawable as BitmapDrawable
+    var clickBitmap : Bitmap = clickBitmapDrawable.bitmap
+
+    init {
+        clickBitmap = Bitmap.createScaledBitmap(clickBitmap,width,height,false)
+    }
+
+    fun onClick() {
+        isClick = true
+        TODO("블록 클릭시 동작 정의, 스킬 블록의 경우 드래그시 스킬범위 표시되도록 구현")
+    }
+
+    override fun draw(canvas: Canvas?) {
+        if(isClick){
+            canvas?.drawBitmap(clickBitmap,x.toFloat(),y.toFloat(),paint)
+        }
+        else {
+            canvas?.drawBitmap(bitmap,x.toFloat(),y.toFloat(),paint)
+        }
+    }
+}
---
+++ app/src/main/java/com/test/rainbowDefense/battle/BlockMenu.kt
@@ -0,0 +1,73 @@
+package com.test.rainbowDefense.battle
+
+import android.content.Context
+import android.graphics.Canvas
+import com.test.rainbowDefense.R
+
+class BlockMenu(
+    val content: Context,
+    val block_array :ArrayList<Block>,
+    val x : Int,
+    val y : Int,
+    val width: Int,
+    val height: Int) {
+    val blockDrawable = content.resources.getDrawable(R.drawable.text_box, content.theme)
+    val blockClickDrawable = content.resources.getDrawable(R.drawable.text_box, content.theme)
+
+    var blockX = x + 10
+    val blockY = y + 10
+    val blockWidth = 230
+    val blockHeight = 220
+
+    init {
+        block_array.add(    // 블록 메뉴 UI 창
+            Block(
+                x,
+                y,
+                width,
+                height,
+                blockDrawable,
+                blockClickDrawable
+            ).apply{
+                isClickable = false
+            }
+        )
+        block_array.add(
+            Block(
+                blockX,
+                blockY,
+                blockWidth,
+                blockHeight,
+                blockDrawable,
+                blockClickDrawable
+            )
+        )
+        blockX += 240
+        block_array.add(
+            Block(
+                blockX,
+                blockY,
+                blockWidth,
+                blockHeight,
+                blockDrawable,
+                blockClickDrawable
+            )
+        )
+        blockX += 240
+        block_array.add(
+            Block(
+                blockX,
+                blockY,
+                blockWidth,
+                blockHeight,
+                blockDrawable,
+                blockClickDrawable
+            )
+        )
+        blockX += 240
+        TODO("블록 메뉴 내부에서 블록을 전부 생성하도록 하고" +
+                "유닛 블록 클릭시 블록 전부 삭제후 유닛 블록만 보이도록 한다. + 뒤로가기 버튼 " +
+                "블록마다 드로어블을 넣어줄 수 있도록 여러가지 비트맵을 가져온다.")
+    }
+
+}
