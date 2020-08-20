674e62547857c6845dd4ffed3666cd5de53826fc
SeoHyeonMyeong <gusaud2199@naver.com>
2020-08-20 15:11:52 +0900

    Battle 관련 코드 정리
    GameManager에 있던 Arrow 관련 함수들과, Effect 관련 함수를 새로운 클래스로 분리
    ArrowManager 클래스에서 화살 관련된 함수를 모두 관리
    EffectManager 클래스에서 이펙트 관련된 함수를 모두 관리

--- app/src/main/java/com/test/rainbowDefense/battle/GameManager.kt
+++ app/src/main/java/com/test/rainbowDefense/battle/GameManager.kt
@@ -15,17 +15,17 @@ class GameManager(val content: Context, val v: CanvasView, wave: WaveEntity) {
     var isRunning = false
 
     // 화면 크기 고정
-    val displayWidth = 1920
-    val displayHeight = 1080
+    private val displayWidth = 1920
+    private val displayHeight = 1080
 
     // 전투화면 높이
-    val battleHeight = 840
+    private val battleHeight = 840
 
     // 메뉴(블록)화면 높이
-    val menuHeight = 240
+    private val menuHeight = 240
 
     // 상태창 높이
-    val statusHeight = 120
+    private val statusHeight = 120
 
     // 핸들러 쓰레드
     var handler: Handler? = Handler()
@@ -39,57 +39,16 @@ class GameManager(val content: Context, val v: CanvasView, wave: WaveEntity) {
     var touchY: Float = 500f
     var isTouch: Boolean = false
 
-    // 블록메뉴, 웨이브, 배경(상태포함) 인스턴스
+    // 블록메뉴, 웨이브, 배경, 이펙트, 화살 인스턴스
     val blockMenu = BlockMenu(content,v.block_array,0,battleHeight,displayWidth,menuHeight)
     val waveManager = WaveManager(content,v,wave,ping,displayWidth,battleHeight)
     val background = Background(displayWidth,battleHeight,statusHeight,v,content)
-
-    // 이펙트(애니메이션) 리소스 (추후 EffectManager 클래스로 옮길 예정)
-    val effectBitmaps = arrayListOf<Bitmap>();
-    val effectId = arrayListOf(
-        R.drawable.blueish_flame_0001,
-        R.drawable.blueish_flame_0002,
-        R.drawable.blueish_flame_0003,
-        R.drawable.blueish_flame_0004,
-        R.drawable.blueish_flame_0005,
-        R.drawable.blueish_flame_0006,
-        R.drawable.blueish_flame_0007,
-        R.drawable.blueish_flame_0008,
-        R.drawable.blueish_flame_0009,
-        R.drawable.blueish_flame_0010,
-        R.drawable.blueish_flame_0011,
-        R.drawable.blueish_flame_0012,
-        R.drawable.blueish_flame_0013,
-        R.drawable.blueish_flame_0014,
-        R.drawable.blueish_flame_0015,
-        R.drawable.blueish_flame_0016,
-        R.drawable.blueish_flame_0017,
-        R.drawable.blueish_flame_0018,
-        R.drawable.blueish_flame_0019,
-        R.drawable.blueish_flame_0020,
-        R.drawable.blueish_flame_0021,
-        R.drawable.blueish_flame_0022,
-        R.drawable.blueish_flame_0023,
-        R.drawable.blueish_flame_0024,
-        R.drawable.blueish_flame_0025,
-        R.drawable.blueish_flame_0026,
-        R.drawable.blueish_flame_0027,
-        R.drawable.blueish_flame_0028,
-        R.drawable.blueish_flame_0029,
-        R.drawable.blueish_flame_0030
-    )
-
-    // 화살 딜레이타임, 카운터 (추후 Arrow 클래스로 옮길 예정)
-    var arrowTime: Int = (0.1 * ping).toInt()
-    var arrowCounter: Int = 0
+    private val effectManager = EffectManager(content,v,ping)
+    val arrowManager = ArrowManager(content,v,effectManager,ping)
 
 
-    // 초기화
+    // 초기화 ( 터치 이벤트 설정 )
     init {
-        effectId.forEach{
-            val bitmap = BitmapFactory.decodeResource(v.resources,it)
-            effectBitmaps.add(Bitmap.createScaledBitmap(bitmap,200,200,false))
-        }
         v.setOnTouchListener { v, event ->
             val x = event.x
             val y = event.y
@@ -119,13 +78,13 @@ class GameManager(val content: Context, val v: CanvasView, wave: WaveEntity) {
     inner class ThreadClass : Thread() {
         override fun run() {
             if(isRunning) {
-                arrowCheck()
-                waveManager.waveCheck()
-                checkProjectile(v.projectile_array)
-                checkCollision(v.monster_array, v.projectile_array)
-                checkEffect(v.effect_array)
+                arrowManager.arrowCheck(isTouch,touchX,touchY)
+                arrowManager.checkProjectile()
+                arrowManager.checkCollision()
+                effectManager.checkEffect()
                 v.monster_array.forEach { it.move() }
                 v.projectile_array.forEach { it.move() }
+                waveManager.waveCheck()
                 v.invalidate()
                 winCheck()
                 handler?.postDelayed(this, 1000 / ping.toLong())
@@ -176,98 +135,6 @@ class GameManager(val content: Context, val v: CanvasView, wave: WaveEntity) {
             }
         }
 //        TODO("건물 또는 유닛 클릭시 정보 표시하는 코드")
-    }
-
-
-    // 투사체, 웨이브 딜레이 체크
-    fun arrowCheck() {
-        if (isTouch && arrowCounter >= arrowTime) {
-            arrowStart()
-            arrowCounter = 0
-        } else {
-            arrowCounter++
-        }
-//        TODO("Arrow 클래스 생성, arrow 관련 함수를 분리 ")
-    }
-    fun arrowStart() {
-        val arrawDrawable = v.resources.getDrawable(R.drawable.arrow, content.theme)
-        v.projectile_array.add(
-            Projectile(
-                10,
-                touchY.toInt(),
-                137,
-                17,
-                arrawDrawable
-            )
-        )
-//        TODO("Arrow 클래스 생성, arrow 관련 함수를 분리 ")
-    }
-
-    // 투사체 수명 체크, 충돌 체크
-    fun checkProjectile(arrayList: ArrayList<Projectile>) {
-        var n: Int = 0
-        while (n < arrayList.size) {
-            if (arrayList[n].lifetime == 0) {
-                arrayList.removeAt(n)
-                n--
-            }
-            n++
-        }
-//        TODO("Arrow 클래스 생성, arrow 관련 함수를 분리 ")
-    }
-    fun checkCollision(monster: ArrayList<Monster>, projectile: ArrayList<Projectile>) {
-        var n: Int = 0
-        var m: Int = 0
-        while (n < monster.size) {
-            while (m < projectile.size) {
-                val condition1: Boolean =
-                    monster[n].x < projectile[m].x + projectile[m].width && monster[n].x + monster[n].width > projectile[m].x
-                val condition2: Boolean =
-                    monster[n].y < projectile[m].y + projectile[m].height && monster[n].y + monster[n].height > projectile[m].y
-                if (condition1 && condition2) {
-                    makeEffect(monster[n].x+monster[n].width/2,monster[n].y+monster[n].height/2)
-                    monster.removeAt(n)
-                    projectile.removeAt(m)
-                    m = 0
-                    break
-                }
-                m++
-            }
-            m = 0
-            n++
-        }
-//        TODO("Arrow 클래스 생성, arrow 관련 함수를 분리 ")
-    }
-
-    // 애니메이션 이펙트 생성, 체크
-    fun makeEffect(x:Int,y:Int){
-        v.effect_array.add(
-            Effect(
-                x - 100,
-                y - 100,
-                200,
-                200,
-                effectBitmaps,
-                30,
-                10
-            )
-        )
-//        TODO("EffectManager 클래스 생성, Effect 관련 함수를 분리 ")
-    }
-    fun checkEffect(effect: ArrayList<Effect>) {
-        var n: Int = 0
-        while (n < effect.size) {
-            effect[n].time += 1000/ping
-            if (effect[n].time>=effect[n].lifetime) {
-                effect.removeAt(n)
-                n--
-            }
-            else {
-                effect[n].bitmap_index = effect[n].time / effect[n].duration
-            }
-            n++
-        }
-//        TODO("EffectManager 클래스 생성, Effect 관련 함수를 분리 ")
     }
 
     // 승리, 패배 조건 확인
