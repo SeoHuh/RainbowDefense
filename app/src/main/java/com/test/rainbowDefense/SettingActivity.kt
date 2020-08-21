0535818ff448dd2782316d677a75af24bb7fc3e2
DESKTOP-F6QVMP9\d00rc <d00rcastle@naver.com>
2020-08-21 18:42:28 +0900

    setting divided into loby and battle setting layout update

--- app/src/main/java/com/test/rainbowDefense/SettingActivity.kt
+++ app/src/main/java/com/test/rainbowDefense/SettingActivity.kt
@@ -1,6 +1,6 @@
 package com.test.rainbowDefense
 
-import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -10,17 +10,19 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.test.rainbowDefense.database.StateEntity
 import com.test.rainbowDefense.database.StateViewModel
-import kotlinx.android.synthetic.main.activity_loby.*
-import kotlinx.android.synthetic.main.activity_setting.*
+import kotlinx.android.synthetic.main.setting_loby.*
 
 class SettingActivity : AppCompatActivity() {
 
     private lateinit var stateViewModel: StateViewModel
 
+    lateinit var perference: SharedPreferences
+    lateinit var editor: SharedPreferences.Editor
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_setting)
+        setContentView(R.layout.setting_loby)
         window.decorView.systemUiVisibility = (
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                         or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
@@ -59,19 +61,11 @@ class SettingActivity : AppCompatActivity() {
             }
         })
 
-
-        val intent = Intent(this, LobyActivity::class.java)
-
-        btn_setting_back.setOnClickListener{
-            super.finish()
-            overridePendingTransition(R.anim.slide_in_bottom, R.anim.slide_out_top)
-        }
-
-
     }
 
     override fun finish() {
         super.finish()
         overridePendingTransition(R.anim.slide_in_bottom, R.anim.slide_out_top)
     }
+
 }
